7cc1356e35bc4746a006449bc3e71ba4ff15bb86
Deep Sheth <shethdeep22@gmail.com>
2023-06-28 02:23:09 +0530

    -Profile Fragment Updated

--- app/src/main/java/com/example/gymattendancetracker/Fragments/ProfileFragment.kt
+++ app/src/main/java/com/example/gymattendancetracker/Fragments/ProfileFragment.kt
@@ -7,10 +7,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
 import com.example.gymattendancetracker.MainActivity
+import com.example.gymattendancetracker.Model.User
 import com.example.gymattendancetracker.R
 import com.example.gymattendancetracker.SignInActivity
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_profile.*
 
@@ -23,8 +29,19 @@ class ProfileFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_profile, container, false)
         val logoutbtn = view.findViewById<Button>(R.id.LogOutBtn)
+        val userName = view.findViewById<TextView>(R.id.userName)
 
+        val userDbRef = FirebaseDatabase.getInstance().getReference("Users").child(Firebase.auth.currentUser!!.uid).child("name")
 
+        userDbRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                userName.text = snapshot.value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                userName.text = error.message
+            }
+        })
 
 
         //logout button
--- app/src/main/java/com/example/gymattendancetracker/Model/User.kt
+++ app/src/main/java/com/example/gymattendancetracker/Model/User.kt
@@ -1,8 +1,28 @@
 package com.example.gymattendancetracker.Model
 
+import java.time.LocalDate
+import java.util.Date
+
+//class User{
+//    var name : String? = null
+//    var phone : String? = null
+//    var email : String? = null
+//    var password : String? = null
+//
+//
+//    constructor(){}
+//
+//    constructor(name : String , phone : String , email : String , password : String){
+//        this.name = name
+//        this.phone = phone
+//        this.email = email
+//        this.password = password
+//    }
+//}
+
 data class User(
-    val name : String ,
-    val phone : String,
-    val email : String,
-    val password : String
+    var name : String,
+    var phone : String,
+    var email : String,
+    var password : String,
 )
